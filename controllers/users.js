b4f6d9fe5e70a59cca9f1833178d1b2706479757
Sergey Kirsanov <qvanchik571@yandex.ru>
2023-12-28 20:33:36 +0300

    pr-13:pull(сдача проекта место, бэкенд; попытка №1)

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,30 @@
+const DefaultError = require('../errors/defaultError');
+const NotFoundError = require('../errors/notFoundError');
+const ValidationError = require('../errors/validationError');
 const Users = require('../models/user');
-
-const VALIDATION_ERROR_CODE = 400;
-const DEFAULT_ERROR_CODE = 500;
-const NOTFOUND_ERROR_CODE = 404;
 
 module.exports.getUsers = async (req, res, next) => {
   await Users.find({})
     .then((users) => res.send({ data: users }))
     .catch(() => {
-      next(res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
+      next(new DefaultError({ message: 'Ошибка по умолчанию' }));
     });
 };
 
 module.exports.getUsersById = async (req, res, next) => {
   await Users.findById(req.params.userId)
     .then((user) => {
-      res.send(user);
+      if (!user) {
+        throw new NotFoundError({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.send(user);
+      }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(res.status(NOTFOUND_ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден' }));
+      if (err.name === 'ValidationError') {
+        throw new ValidationError({ message: 'Переданы некорректные данные при создании пользователя' });
       } else {
-        next(res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
+        next(new DefaultError({ message: 'Ошибка по умолчанию' }));
       }
     });
 };
@@ -30,14 +33,12 @@ module.exports.createUser = async (req, res, next) => {
   const { name, about, avatar } = req.body;
 
   await Users.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then(() => res.send({ data: name, about, avatar }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(res.status(VALIDATION_ERROR_CODE).send({
-          message: 'Переданы некорректные данные при создании пользователя',
-        }));
+        throw new ValidationError({ message: 'Переданы некорректные данные при создании пользователя' });
       } else {
-        next(res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
+        next(new DefaultError({ message: 'Ошибка по умолчанию' }));
       }
     });
 };
@@ -53,15 +54,17 @@ module.exports.patchProfile = async (req, res, next) => {
     },
   )
     .then((user) => {
-      next(res.send(user));
+      if (!user) {
+        throw new NotFoundError({ message: 'Пользователь с указанным _id не найден' });
+      } else {
+        res.send(user);
+      }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(NOTFOUND_ERROR_CODE).send({ messsge: 'Пользователь с указанным _id не найден' });
-      } else if (err.name === 'ValidationError') {
-        next(res.status(VALIDATION_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' }));
+      if (err.name === 'ValidationError') {
+        next(new ValidationError({ message: 'Переданы некорректные данные при обновлении профиля' }));
       } else {
-        next(res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
+        next(new DefaultError({ message: 'Ошибка по умолчанию' }));
       }
     });
 };
@@ -77,15 +80,17 @@ module.exports.patchAvatar = async (req, res, next) => {
     },
   )
     .then((user) => {
-      next(res.send(user));
+      if (!user) {
+        throw new NotFoundError({ message: 'Пользователь с указанным _id не найден' });
+      } else {
+        res.send(user);
+      }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(NOTFOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-      } else if (err.name === 'ValidationError') {
-        next(res.status(VALIDATION_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' }));
+      if (err.name === 'ValidationError') {
+        next(new ValidationError({ message: 'Переданы некорректные данные при обновлении аватара' }));
       } else {
-        next(res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' }));
+        next(new DefaultError({ message: 'Ошибка по умолчанию' }));
       }
     });
 };
