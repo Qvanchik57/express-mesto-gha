4dc479ff46f46314c81bc784e805350962f10333
Sergey Kirsanov <qvanchik571@yandex.ru>
2024-01-15 03:40:28 +0300

    pr-14:pull(сдача проекта место, бэкенд; попытка №5)

--- models/user.js
+++ models/user.js
@@ -6,15 +6,15 @@ const NotAuthError = require('../errors/notAuthError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
+    default: 'Исследователь океанов',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Исследователь океанов',
   },
   avatar: {
     type: String,
